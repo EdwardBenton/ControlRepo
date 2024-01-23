87e18e65da2859d671cba71b53a7d99d77dcdc7e
EdwardBenton <ed@hikersblog.co.uk>
2024-01-23 16:41:44 +0000

    Reverted classes declaration

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,6 +1,8 @@
 # The base profile should include component modules that will be on all nodes
-class profile::base {
-if $data_option == 1 {
+class profile::base (
+Boolean $data_option
+) {
+if $data_option == true {
   include security_fixes::cipher_settings
 }
-}
+  }
