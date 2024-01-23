61c2e8649e7b207c1e92cb9f73d8790e3600dbbf
EdwardBenton <ed@hikersblog.co.uk>
2024-01-23 16:51:25 +0000

    .

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,4 +1,8 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
   include security_fixes::cipher_settings
+  file { 'c:/windows/temp/base_pp_did_this.sh':
+    ensure => file,
+    content => 'Added from base.pp',
+}
   }
