964a29836d9a41a626ad28297679b810a00d88cb
EdwardBenton <ed@hikersblog.co.uk>
2024-01-23 16:18:06 +0000

    Corrected flawed spelling for data_option

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,6 +1,6 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
-if $data_options == 1 {
+if $data_option == 1 {
   include security_fixes::cipher_settings
 }
 }
