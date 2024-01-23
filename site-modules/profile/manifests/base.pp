f5ecb205d1ff027db57b742be6f8681f058414ca
EdwardBenton <ed@hikersblog.co.uk>
2024-01-23 16:48:22 +0000

    .

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,8 +1,4 @@
 # The base profile should include component modules that will be on all nodes
-class profile::base (
-Boolean $data_option
-) {
-if $data_option == true {
+class profile::base {
   include security_fixes::cipher_settings
-}
   }
