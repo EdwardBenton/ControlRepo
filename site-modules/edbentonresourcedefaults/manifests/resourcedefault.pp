07b2fe493f3f46108bfe7ab2ba042f274ad3245b
EdwardBenton <ed@hikersblog.co.uk>
2024-02-07 17:44:13 +0000

    .

---
+++ site-modules/edbentonresourcedefaults/manifests/resourcedefault.pp
@@ -0,0 +1,12 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include edbentonResourceDefaults::resourcedefault
+class edbentonResourceDefaults::resourcedefault {
+  File {
+    mode => '777',
+    owner => 'root',
+  }
+}
