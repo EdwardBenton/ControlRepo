d1e9b64c2f7d78c0d75512cd36c965b40c90e6c9
EdwardBenton <ed@hikersblog.co.uk>
2024-02-07 17:17:03 +0000

    module test

---
+++ site-modules/edbentonsetdefault/manifests/default.pp
@@ -0,0 +1,11 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include edbentonsetdefault::default
+class edbentonsetdefault::default {
+  File {
+    owner => 'nobody',
+  }
+}
