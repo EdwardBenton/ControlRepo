77a580c41a647ab06cd57bcb1bfdbaf869ed753b
EdwardBenton <ed@hikersblog.co.uk>
2024-02-27 13:49:17 +0000

    .

--- site-modules/edbentonresourcedefaults/manifests/resourcedefault.pp
+++ site-modules/edbentonresourcedefaults/manifests/resourcedefault.pp
@@ -8,12 +8,12 @@ class edbentonresourcedefaults::resourcedefault {
 # epp(<FILE REFERENCE>, [<PARAMETER HASH>])
 $myusers = {
   'nick' => {
-    uid    => lookup('nick_uid', Integer, '1'),
+    uid    => lookup('nick_uid', Integer, "first", '1'),
     gid    => 'allstaff',
     groups => ['developers', 'operations', 'release'],
   },
   'dan'  => {
-    uid    => lookup('dan_uid', Integer, '1'),
+    uid    => lookup('dan_uid', Integer, "first", '1'),
     gid    => 'allstaff',
     groups => ['developers', 'prosvc', 'release'],
   },
