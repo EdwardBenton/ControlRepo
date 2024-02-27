4a4358b7cdf8e33913d49043deac005cd455df36
EdwardBenton <ed@hikersblog.co.uk>
2024-02-27 13:35:15 +0000

    .

--- site-modules/edbentonresourcedefaults/manifests/resourcedefault.pp
+++ site-modules/edbentonresourcedefaults/manifests/resourcedefault.pp
@@ -6,9 +6,15 @@
 #   include edbentonResourceDefaults::resourcedefault
 class edbentonresourcedefaults::resourcedefault {
 # epp(<FILE REFERENCE>, [<PARAMETER HASH>])
-file { '/etc/birthday.conf':
-  ensure  => file,
-  content => epp('edbentonresourcedefaults/filetemplate.epp', {'name' => 'aaron', 'age' => "25"}),
-  # Loads /etc/puppetlabs/code/environments/production/modules/ntp/templates/ntp.conf.epp
-}
+$myusers = {
+  'nick' => { 
+    uid    => lookup('nick_uid', Integer, 'default_uid_value'),
+    gid    => 'allstaff',
+    groups => ['developers', 'operations', 'release'],
+  },
+  'dan'  => { 
+    uid    => lookup('dan_uid', Integer, 'default_uid_value'),
+    gid    => 'allstaff',
+    groups => ['developers', 'prosvc', 'release'],
+  },
 }
