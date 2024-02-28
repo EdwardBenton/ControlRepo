b2de8a0afe21278ee956e04e780b47a6b9c752a5
EdwardBenton <ed@hikersblog.co.uk>
2024-02-28 15:58:16 +0000

    Scripted code manager update

--- manifests/site.pp
+++ manifests/site.pp
@@ -31,7 +31,5 @@ node default {
 #lookup('classes', {merge => unique}).include
   include edbentonresourcedefaults::resourcedefault
   include edbentonresourcedefaults::filetest
-  class {'::puppet_agent':
-  package_version => '7.23.0',
-}
+  include edbentonResourceDefaults::puppetagent
 }
---
+++ site-modules/edbentonresourcedefaults/manifests/upgradeagent.pp
@@ -0,0 +1,9 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include edbentonResourceDefaults::upgradeagent
+class edbentonResourceDefaults::puppetagent {
+  package_version => '7.23.0',
+}
