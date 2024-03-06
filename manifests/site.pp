2347ac85c347bf52b1f81801c24ae07e84c98f54
EdwardBenton <ed@hikersblog.co.uk>
2024-03-06 16:41:38 +0000

    Scripted code manager update

--- manifests/site.pp
+++ manifests/site.pp
@@ -32,4 +32,20 @@ node default {
   include edbentonresourcedefaults::resourcedefault
   include edbentonResourceDefaults::dnffailbail
   include edbentonResourceDefaults::puppet_agent
+  $agent_settings = [
+{ section => 'main', setting => 'statettl', value => $statettl },
+{ section => 'main', setting => 'runtimeout', value => '0' },
+{ section => 'agent', setting => 'splay', value => 'true' },
+{ section => 'main', setting => 'exclude_unchanged_resources', value => 'true' },
+{ section => 'main', setting => 'usecacheonfailure', value => 'false' },
+{ section => 'agent', setting => 'crl_refresh_interval', value => '1d' },
+{ section => 'main', setting => 'environment', value => $server_facts['environment'] },
+{ section => 'main', setting => 'graph', value => 'true' },
+{ section => 'main', setting => 'http_read_timeout', value => $http_read_timeout },
+]
+
+class { 'puppet_agent':
+config => $agent_settings,
+notify => Service['puppet'],
+}
 }
