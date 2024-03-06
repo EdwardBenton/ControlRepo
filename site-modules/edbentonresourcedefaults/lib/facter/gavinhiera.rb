f413fae99c438c6aa28c4318cc61e01afd691605
EdwardBenton <ed@hikersblog.co.uk>
2024-03-06 13:19:55 +0000

    Scripted code manager update

--- site-modules/edbentonresourcedefaults/lib/facter/gavinhiera.rb
+++
@@ -1,5 +0,0 @@
-Facter.add('GavinHiera') do
-    setcode do
-      Facter::Core::Execution.execute('puppet lookup edbentonresourcedefaults::dan_uid --environment production --node $(hostname -f)')
-    end
-  end
