444ab251fc1f059f715e0f695948dab03fdaee22
EdwardBenton <ed@hikersblog.co.uk>
2024-03-12 15:00:59 +0000

    Scripted code manager update

--- site-modules/edbentonresourcedefaults/lib/puppet/functions/example_sensitive_function.rb
+++ site-modules/edbentonresourcedefaults/lib/puppet/functions/example_sensitive_function.rb
@@ -2,7 +2,7 @@ Puppet::Functions.create_function(:example_sensitive_function) do
     dispatch :should_to_s do
       param 'Sensitive', :newvalue
     end
-  
+
     def should_to_s(newvalue)
       if @resource[:show_diff] == :true && Puppet[:show_diff]
         newvalue
@@ -12,13 +12,12 @@ Puppet::Functions.create_function(:example_sensitive_function) do
         Sensitive('[redacted sensitive information]')
       end
     end
-  
+
     dispatch :is_to_s do
       param 'Sensitive', :value
     end
-  
+
     def is_to_s(value)
       should_to_s(value)
     end
   end
-  
