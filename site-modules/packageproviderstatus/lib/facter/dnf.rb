4ef270abf045c8fde71bb761d3a232b49ba88d21
EdwardBenton <ed@hikersblog.co.uk>
2024-04-04 10:42:14 +0100

    Scripted code manager update

--- site-modules/packageproviderstatus/lib/facter/dnf.rb
+++ site-modules/packageproviderstatus/lib/facter/dnf.rb
@@ -1,7 +1,6 @@
 Facter.add('dnfavailable') do
     confine :kernel => 'Linux'
     setcode do
-      result = Facter::Core::Execution.execute('dnf --version > /dev/null 2>&1', on_fail: false)
-      result ? 'true' : 'false'
+    Facter::Core::Execution.execute('dnf --version > /dev/null 2>&1', on_fail: false)
     end
   end
