cecf9cc18e28c0abdc2b13b8dbe3c5501a8f2dad
EdwardBenton <ed@hikersblog.co.uk>
2024-04-04 10:29:58 +0100

    Scripted code manager update

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,6 +29,5 @@ node default {
   # Example:
   #   class { 'my_class': }
 #lookup('classes', {merge => unique}).include
-include edbentonresourcedefaults::resourcedefault
 include edbentonresourcedefaults::addusertest
 }
