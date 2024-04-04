1a2ac42134da6db079d58deac2beef049c834996
EdwardBenton <ed@hikersblog.co.uk>
2024-04-04 13:53:03 +0100

    Scripted code manager update

--- site-modules/packageproviderstatus/lib/facter/dnfavailable.rb
+++ site-modules/packageproviderstatus/lib/facter/dnfavailable.rb
@@ -2,6 +2,11 @@ Facter.add('dnfavailable') do
   confine :osfamily => 'RedHat'
   setcode do
     dnf_path = Facter::Core::Execution.which('dnf')
-    dnf_path.nil? ? false : true
+    if dnf_path.nil?
+      false
+    else
+      dnf_config_check = Facter::Core::Execution.execute('dnf check', on_fail: false)
+      dnf_config_check.strip.empty? ? true : false
+    end
   end
 end
