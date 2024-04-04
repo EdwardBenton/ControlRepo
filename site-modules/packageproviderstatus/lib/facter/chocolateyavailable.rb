3fb24ac88968fa7776bda86811e8ee6b43609fde
EdwardBenton <ed@hikersblog.co.uk>
2024-04-04 14:04:24 +0100

    Scripted code manager update

--- site-modules/packageproviderstatus/lib/facter/chocolateyavailable.rb
+++
@@ -1,8 +0,0 @@
-Facter.add('chocolateyavailable') do
-    confine :osfamily => 'windows'
-    setcode do
-      chocolatey_path = Facter::Core::Execution.which('choco')
-      !chocolatey_path.nil?
-    end
-  end
-  
