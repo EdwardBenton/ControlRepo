4574876697af6f9ea5c778f1f15371018b95181a
EdwardBenton <ed@hikersblog.co.uk>
2024-05-10 11:41:33 +0100

    Scripted code manager update

--- site-modules/edbentonresourcedefaults/manifests/filetest.pp
+++ site-modules/edbentonresourcedefaults/manifests/filetest.pp
@@ -6,5 +6,5 @@
 #   include edbentonResourceDefaults::filetest
 class edbentonresourcedefaults::filetest {
 $blockdevice_sda_size_type = type($::blockdevice_sda_size)
-notice("The data type of blockdevice_sda_size is: ${blockdevice_sda_size_type}")
+notify("The data type of blockdevice_sda_size is: ${blockdevice_sda_size_type}")
 }
