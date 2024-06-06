a671b44c605ac004f83808c96d2a6dcba0426717
EdwardBenton <ed@hikersblog.co.uk>
2024-06-06 13:43:41 +0100

    Scripted code manager update

--- site-modules/edbentonresourcedefaults/manifests/filetest.pp
+++ site-modules/edbentonresourcedefaults/manifests/filetest.pp
@@ -6,7 +6,10 @@
 #   include edbentonResourceDefaults::filetest
 class edbentonresourcedefaults::filetest {
 # revert change to check whether deprecate warnings are raised
-$rnd_pwd = stdlib::seeded_rand_string(12, $facts['networking']['hostname'])
+$randomer = seeded_rand_string(300, 'my_seed')
+notify { 'output':
+message => $randomer,
+}
 #$rnd_pwd2 = seeded_rand_string(12, $facts['networking']['hostname'])
 if $stdlib_log_deprecations {
   notify { "STDLIB_LOG_DEPRECATIONS is set to ${stdlib_log_deprecations}": }
