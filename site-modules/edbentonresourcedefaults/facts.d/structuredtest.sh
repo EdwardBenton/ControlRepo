94e16ec9ec8927c2daba8b0c16a74a4c3586b371
EdwardBenton <ed@hikersblog.co.uk>
2024-06-12 12:43:37 +0100

    Scripted code manager update

--- site-modules/edbentonresourcedefaults/facts.d/structuredtest.sh
+++ site-modules/edbentonresourcedefaults/facts.d/structuredtest.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-# JSON DONOVAN because why not?
-echo "{"
-echo "  \"structuredtest\": {"
-echo "    \"key1\": \"value1\","
-echo "    \"key2\": \"value2\""
-echo "  }"
-echo "}"
+echo name=Ed
+echo city=Belfast
+echo brand=Puppet
