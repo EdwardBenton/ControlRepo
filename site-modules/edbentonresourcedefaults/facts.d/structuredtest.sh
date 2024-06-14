3e52bd5485aaad1cb72674286a33c082a6fb1db9
EdwardBenton <ed@hikersblog.co.uk>
2024-06-14 13:02:57 +0100

    Scripted code manager update

--- site-modules/edbentonresourcedefaults/facts.d/structuredtest.sh
+++ site-modules/edbentonresourcedefaults/facts.d/structuredtest.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-echo structuredname=Ed
-echo structuredcity=Belfast
-echo structuredbrand=Puppet
+
+# Define the key-value pairs
+key1="value1"
+key2="value2"
+
+# Output the fact in JSON format
+echo "{"
+echo "\"structured\": {"
+echo "\"key1\": \"$key1\","
+echo "\"key2\": \"$key2\""
+echo "}"
+echo "}"
