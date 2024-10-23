2baf885813d35de7cd776c1d3ca981a9c4420b09
EdwardBenton <ed@hikersblog.co.uk>
2024-10-23 14:13:55 +0100

    Scripted code manager update

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 git add .
 git commit -m "Scripted code manager update"
 git push origin
-ssh -i ~/.ssh/id_rsa-acceptance -p 22 user@10.253.22.105 "puppet code deploy --all --wait"
+ssh -i ~/.ssh/id_rsa-acceptance -p 22 user@10.253.3.128 "puppet code deploy --all --wait"
